5ed06fe2b404f524e18503e298dbf74cb6ad390d
kholoud840 <kholoudameen@gmail.com>
2018-02-08 20:34:05 +0200

    add filters and some changes

--- js/app.js
+++ js/app.js
@@ -5,4 +5,28 @@ $(".toggle").on("click" , function(){
 });
 
 
+
+    $(".c-filter .c-filter__body").hide();
+
+    $(".c-filter .c-filter__title").on("click", function () {
+        var thisFilter = $(this).parents(".c-filter");
+        var wasOpen = thisFilter.hasClass('open');
+        closeFilter($(".c-filter.open"));
+
+        if (!wasOpen) {
+            openFilter(thisFilter);
+        }
+    });
+
+    var openFilter = function(filter) {
+        filter.addClass("open")
+        filter.focus();
+        filter.find(".c-filter__body").stop().slideDown(400);
+        filter.find(".c-filter__body").stop().slideDown(400);
+    };
+
+    var closeFilter = function(filter) {
+        filter.removeClass("open");
+        filter.find(".c-filter__body").stop().slideUp(400);
+    };
 });
